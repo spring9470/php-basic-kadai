a0d8461f7a2edc731230d1f761e02883c71467c9
spring9470 <168796258+spring9470@users.noreply.github.com>
2024-07-27 14:32:23 +0900

    kadai_007をアップデート

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $personal_data = ['name' => 'onion' ,'price' => 200 ,'weight' => 160];
+    // 連想配列の値を出力する
+    print_r($personal_data);
+    
+    ?>
+  </p>
+  
+</body>
+</html>
